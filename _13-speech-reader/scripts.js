099130554a3d75936046a2fef94c082853f61655
Dani <nikiforovdaniel@gmail.com>
2021-07-23 23:36:42 -0300

    Finalizando funcionalidad

--- _13-speech-reader/scripts.js
+++ _13-speech-reader/scripts.js
@@ -76,10 +76,87 @@ function createBox(item) {
             ${text}
         </p>`;
            
-    // Evento de hablar
+    $box.addEventListener('click', (e) => {
+        console.log()
+        setTextMessage(text);
+        speakText();
+
+        // Añadir un efecto de elemento activo
+        $box.classList.add('active');
+        setTimeout(() => $box.classList.remove('active'), 800)
+    })
     DOM.main.appendChild($box)
 }
 
+// Iniciar speech synth
+const message = new SpeechSynthesisUtterance();
+
+// Almacenar voces
+let voices = [];
+
+function getVoices() {
+    voices = speechSynthesis.getVoices();
+
+    voices.forEach(voice => {
+        const option = document.createElement('OPTION');
+        
+        option.value = voice.name;
+        option.innerText = `${voice.name} ${voice.lang}`
+
+        DOM.voiceSelect.appendChild(option)
+    })
+}
 
 
+// Establece texto a leer
+function setTextMessage(text) {
+    message.text = text;
+}
 
+
+// Elegir Voz
+function setVoice(e) {
+    message.voice = voices.find(voice => voice.name === e.target.value)
+}
+
+// Leer el texto
+function speakText() {
+    speechSynthesis.speak(message)
+}
+
+// Toggle text box
+
+DOM.toggleBtn.addEventListener('click', () => document.getElementById('text-box').classList.toggle('show'))
+
+// Cerrar text box
+
+DOM.closeBtn.addEventListener('click', () => document.getElementById('text-box').classList.toggle('show'))
+
+
+// Cambiar voces
+
+speechSynthesis.addEventListener('voiceschanged', getVoices);
+
+DOM.voiceSelect.addEventListener('change', setVoice)
+
+
+getVoices();
+
+
+// Boton de leer texto
+
+DOM.readBtn.addEventListener('click',() => {
+    setTextMessage(DOM.textArea.value);
+    speakText()
+} )
+
+
+// BOQUITA
+
+
+function reproducirAudio () {
+   const x = document.getElementById('cancion');
+   x.play()
+   setTimeout(() => x.pause(), 25000)
+    
+}
